142534765ddc53f3ca71475eb4d9b8b7d1501872
ShowBaba <samwise858@gmail.com>
2022-06-28 13:33:37 +0100

    update

--- lazerpay_test.go
+++ lazerpay_test.go
@@ -30,13 +30,7 @@ func TestRequests(t *testing.T) {
 	setup()
 
 	t.Run("GET Request", func(t *testing.T) {
-		p := payment.New(config)
-		args := payment.NewInitPaymentReq("YOUR_REFERENCE", "Samuel Shoyemi", "samwise858@gmail.com", "USDT", "USD", 100, true, map[string]string{"type": "Wallet fund"})
-		resp, err := p.InitializePayment(args)
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Println(resp)
+		
 	})
 
 	t.Run("POST Request", func(t *testing.T) {
